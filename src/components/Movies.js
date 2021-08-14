0ffa512cb5aa1fb5087de5ce55e61432a95207a4
ThaerJomhawi <thaer.fjomhawi@gmail.com>
2021-08-14 16:47:49 +0300

    update

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,14 +5,15 @@ import ListGroupItem from "react-bootstrap/ListGroupItem";
 
 class Movies extends Component {
   render() {
+   
     return (
       <div>
-        <Card style={{ width: "30rem" }}>
+        <Card style={{ width: "30rem", marginBottom: "10px" }}>
           <Card.Img
             variant="top"
             src={this.props.imageUrl}
             alt="Map"
-            width="300px"
+            
           />
 
           <Card.Body>
@@ -21,7 +22,7 @@ class Movies extends Component {
           <ListGroup className="list-group-flush">
             <ListGroupItem>Over View: {this.props.overview}</ListGroupItem>
             <ListGroupItem>Avg Votes: {this.props.averageVotes}</ListGroupItem>
-            <ListGroupItem>Total Votes: {this.props.totalVotes}</ListGroupItem>
+            <ListGroupItem>Total Votes: {this.props.vote_count}</ListGroupItem>
             <ListGroupItem>Popularity: {this.props.popularity}</ListGroupItem>
             <ListGroupItem>Released: {this.props.releasedOn}</ListGroupItem>
           </ListGroup>
